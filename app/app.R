a0abb952e9d57ab5498f3a5d5f793b16d568b52b
stesiam <52320488+stesiam@users.noreply.github.com>
2022-10-06 14:31:49 +0300

    Add distributions according to their type

--- app/app.R
+++ app/app.R
@@ -8,11 +8,47 @@ header = dashboardHeader(title = "Shiny Distributions", titleWidth = 230)
 
 ## Sidebar
 
-sidebar = dashboardSidebar()
+sidebar = dashboardSidebar(
+  selectInput("type_of_distribution", "Select type of Distribution:",
+              c("Discrete" = "dis",
+                "Continuous" = "cont")),
+  
+  
+  # Conditional Panel for Discrete Distributions
+  
+  conditionalPanel(
+    condition = "input.type_of_distribution == 'dis'",
+    selectInput("discrete_dist", "Select Distribution:",
+                c("Bernoulli" = "bernoulli",
+                  "Binomial" = "binom",
+                  "Poisson" = "poisson",
+                  "Geometric" = "geom",
+                  "Negative Binomial (0)" = "negbinom0",
+                  "Negative Binomial (1)" = "negbinom1",
+                  "Hypergeometric" = "hypergeom",
+                  "Discrete Uniform" = "dunif")),
+  ),
+  
+  # Conditional Panel for Continuous Distributions
+  
+  conditionalPanel(
+    condition = "input.type_of_distribution == 'cont'",
+    selectInput("contin_distributions", "Select Distribution:",
+                c("Normal" = "normal",
+                  "Uniform" = "uniform",
+                  "Expenential" = "exp",
+                  "Gamma" = "gamma",
+                  "Beta" = "beta",
+                  "Cauchy" = "cauchy",
+                  "Weibull" = "weibull")),
+  )
+)
 
 ## Main panel
 
 body  = dashboardBody()
+
+
 
 ## Gather all the components
 ui <- dashboardPage( 
