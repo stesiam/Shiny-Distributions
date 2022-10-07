b6e3d40bcfda5d9427ca22105b2727b9e220cb32
stesiam <52320488+stesiam@users.noreply.github.com>
2022-10-07 11:15:56 +0300

    Conditional panels for parameters of each  discrete distribution

--- app/app.R
+++ app/app.R
@@ -27,7 +27,76 @@ sidebar = dashboardSidebar(
                   "Negative Binomial (1)" = "negbinom1",
                   "Hypergeometric" = "hypergeom",
                   "Discrete Uniform" = "dunif")),
+    
+    ### Conditional Panels for each distribution
+    
+    ### Bernoulli,
+    
+    conditionalPanel(
+      condition = "input.discrete_dist == 'bernoulli'",
+      sliderInput("bernoulli_p", "Probability", min = 0, max = 1, value = 0.5)),
+    
+   ## Binomial 
+   
+   conditionalPanel(
+     condition = "input.discrete_dist == 'binom'",
+     numericInput("n_binom", "Number of events" , value = 2),
+     sliderInput("binom_p", "Probability", min = 0, max = 1, value = 0.5)),
+   
+   
+   ## Poisson
+   
+   conditionalPanel(
+     condition = "input.discrete_dist == 'poisson'",
+     numericInput("lambda", "Lambda" , value = 2)),
+   
+   ## Geometric
+   
+   conditionalPanel(
+     condition = "input.discrete_dist == 'geom'",
+     numericInput("n_binom", "p" , value = 0.5)),
+   
+   
+   ## NB0
+   
+   conditionalPanel(
+     condition = "input.discrete_dist == 'negbinom0'",
+     numericInput("r1", "r" , value = 0.5),
+     numericInput("p1", "p" , value = 0.5)),
+   
+   
+   
+   ## NB1
+   
+   
+   conditionalPanel(
+     condition = "input.discrete_dist == 'negbinom1'",
+     numericInput("r1", "r" , value = 0.5),
+     numericInput("p1", "p" , value = 0.5)),
+   
+   
+   ## Hypergeometric
+   
+   conditionalPanel(
+     condition = "input.discrete_dist == 'hypergeom'",
+     numericInput("r1", "r" , value = 0.5),
+     numericInput("p1", "p" , value = 0.5),
+     numericInput("r1", "r" , value = 0.5)),
+   
+   
+   ## Discrete Uniform
+   
+   conditionalPanel(
+     condition = "input.discrete_dist == 'dunif'",
+     sliderInput("a_dunif", "a" , value = 10, min = 1, max = 45),
+     sliderInput("b_dunif", "b" , value = 16, min = 1, max = 100))
+   
+   
+    
   ),
+  
+  
+  
   
   # Conditional Panel for Continuous Distributions
   
